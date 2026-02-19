05a254aff3dbe6c0a3e5ecf3ee97353a5e815490
scott8911 <scott_github@hotmail.com>
2026-02-19 22:01:43 +0800

    Refactor openMove to use Bilibili player

--- script.js
+++ script.js
@@ -4,31 +4,29 @@ function goTo(screenId) {
     });
     document.getElementById(screenId).classList.add('active');
 }
-
 function openMove(moveName) {
     const moveTitle = document.getElementById('moveTitle');
-    const videoButton = document.getElementById('playVideo');
+    const videoContainer = document.getElementById('videoContainer');
+    const biliPlayer = document.getElementById('biliPlayer');
 
     moveTitle.innerText = moveName.toUpperCase();
 
-    let videoSrc = '';
-    if (moveName === 'The Wop') {
-        videoSrc = 'videos/move1.mp4';
-    } else if (moveName === 'Smurf') {
-        videoSrc = 'videos/move2.mp4';
-    } else {
-        videoSrc = '';
+    let videoUrl = '';
+
+    if (moveName === 'Down Bounce') {
+        videoUrl = 'https://player.bilibili.com/player.html?bvid=BV1K4ZiB9EAc&page=1';
     }
 
-    videoButton.setAttribute('data-src', videoSrc);
-
-    const videoElement = document.getElementById('demoVideo');
-    videoElement.style.display = 'none';
-    videoElement.pause();
-    videoElement.currentTime = 0;
+    if (videoUrl) {
+        biliPlayer.src = videoUrl;
+        videoContainer.style.display = 'block';
+    } else {
+        videoContainer.style.display = 'none';
+    }
 
     goTo('moveDetail');
 }
+
 
 function playVideo(btn) {
     const video = document.getElementById('demoVideo');
