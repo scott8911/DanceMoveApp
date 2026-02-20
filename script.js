4d53775e1f180e419106de07f3ea7b3ab9fb01ad
scott8911 <scott_github@hotmail.com>
2026-02-20 11:00:09 +0800

    Refactor video handling in openMove function
    Updated openMove function to use video element and source. Added setSpeed and toggleLoop functions for video control.

--- script.js
+++ script.js
@@ -7,15 +7,28 @@ function goTo(screenId) {
 
 function openMove(moveName) {
     const moveTitle = document.getElementById('moveTitle');
-    const player = document.getElementById('cloudinaryPlayer');
+    const videoSource = document.getElementById('videoSource');
+    const video = document.getElementById('demoVideo');
 
     moveTitle.innerText = moveName.toUpperCase();
 
     if (moveName === 'Down Bounce') {
-        player.src = "https://player.cloudinary.com/embed/?cloud_name=dpde5dep1&public_id=move1_gethq6";
-    } else {
-        player.src = "";
+        videoSource.src = "https://res.cloudinary.com/dpde5dep1/video/upload/move1_gethq6.mp4";
     }
 
+    video.load();
+    video.playbackRate = 1;
+    video.loop = false;
+
     goTo('moveDetail');
+}
+
+function setSpeed(rate) {
+    const video = document.getElementById('demoVideo');
+    video.playbackRate = rate;
+}
+
+function toggleLoop() {
+    const video = document.getElementById('demoVideo');
+    video.loop = !video.loop;
 }
