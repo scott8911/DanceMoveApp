d824074c415bd6b240131b1d2d2324169881da30
scott8911 <scott_github@hotmail.com>
2026-02-20 11:27:37 +0800

    Fix video source loading in openMove function
    Updated video source selection in openMove function.

--- script.js
+++ script.js
@@ -12,12 +12,13 @@ function openMove(moveName) {
     moveTitle.innerText = moveName.toUpperCase();
 
     if (moveName === 'Down Bounce') {
-        video.src = "https://res.cloudinary.com/dpde5dep1/video/upload/move1_gethq6.mp4";
+        video.querySelector('source').src =
+            "https://res.cloudinary.com/dpde5dep1/video/upload/move1_gethq6.mp4";
     }
 
+    video.load();              // reload with new source
     video.playbackRate = 1;
     video.loop = false;
-    video.load();
 
     goTo('moveDetail');
 }
@@ -31,3 +32,4 @@ function toggleLoop() {
     const video = document.getElementById('cloudinaryPlayer');
     video.loop = !video.loop;
 }
+
